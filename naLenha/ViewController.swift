193f0b344fe43428fbb93ae4dfbf052c401dcbc5
Nicolas de Andrade dos Santos <nicolas.a.santos09@gmail.com>
2018-08-02 14:46:22 -0400

    Initial Commit

---
+++ naLenha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  naLenha
+//
+//  Created by Nicolas de Andrade dos Santos on 02/08/2018.
+//  Copyright © 2018 Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
